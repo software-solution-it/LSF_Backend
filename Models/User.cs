93febe0f9fcd63eb30bc44b5a97512cb8561e0b1
DESKTOP-01ILGOI\conta <contato@software-solution-it.com>
2024-06-17 13:38:49 -0300

    isActive User

--- Models/User.cs
+++ Models/User.cs
@@ -18,6 +18,7 @@ namespace LSF.Models
         public string? Password { get; set; }
         public byte[]? UserImage { get; set; }
         public virtual ICollection<Project>? Projects { get; set; }
+        public bool isActive { get; set; }
         public int? RecoveryCode { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public virtual ICollection<UserRole>? UserRoles { get; set; }
